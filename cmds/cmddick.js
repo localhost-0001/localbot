041dfa43e533e6466fb7e7b1cbfcffa3653d9d09
localhost-0001 <37961130+localhost-0001@users.noreply.github.com>
2018-03-31 21:48:27 +0000

    THE BEST COMMAND EEEEEEEEEEEEEEEEEEEEEEEVER!

---
+++ cmds/cmddick.js
@@ -0,0 +1,18 @@
+const fs     = require('fs')
+const Embeds =  require('../embed.js')
+const config = JSON.parse(fs.readFileSync('./config.json', 'utf8'))
+
+module.exports.run = async (msg, args) => {
+    console.log(`Dick has been run by ${msg.author.username}#${msg.author.discriminator} (ID: "${msg.author.id}")` )
+    msg.channel.send('dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick dick')
+    
+}
+
+module.exports.help = {
+    name: "dick",
+
+}
+
+
+
+
